74e8d742cfa9760d5f79e7df36cdac28f350bba6
aselsche <84043141+aselsche@users.noreply.github.com>
2021-06-22 17:29:56 -0400

    Updates in the comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,15 @@
-# Modules to be able to opent the CSV files:
+# Modules 
 import os
 import csv
 
 # Set path to the financial data budget_data.csv
 csvpath = os.path.join(".","Resources", "election_data.csv") 
 
+# Setup variables
 candidates={} # Initializing candidates as a dictionary so we can use key and value to capture the data.
 total_num_votes = 0 # Initializng the variable to be able to count the rows.
 
-def calc_perc(val,total): #Defining a function to calculate the percentage of the total votes and formatting it
+def calc_perc(val,total): #Defining a function to calculate the percentage of the total votes
     return f"{round(val/total*100, 3)} %"
 
 with open(csvpath) as csvfile: # Open the CSV file
@@ -21,17 +22,19 @@ with open(csvpath) as csvfile: # Open the CSV file
         #     candidates[row[2]] = candidates[row[2]] + 1
         # else:
         #     candidates[row[2]]=1 #initializing the key for specific candidate
-        candidates[row[2]] = candidates.get(row[2],0) + 1 #this is a shorter way of above
+        candidates[row[2]] = candidates.get(row[2],0) + 1 #this is a shorter way of above.
+        #Here I am using .get method to return the value of the item with the speciffied key. 
         total_num_votes = total_num_votes+1
 
 winner=["No Name",0] #setting up winner variable to track who got the most votes.
-all_candidates=""
-for name, votes in candidates.items(): #iterating using .items to find the winner
+all_candidates="" #setting up a variable for all candidates as a string.
+for name, votes in candidates.items(): #iterating using .items to find the winner.
     all_candidates +=f"{name} : {calc_perc(votes,total_num_votes)} ({votes})\n"
     if votes > winner[1]: #using if statement to check if current item has more votes than previous item with most votes
-        winner=[name, votes]
+        winner=[name, votes] #then spit out the winner name and the amount of votes.
 
-election_results= f"""
+# setting up a variable as a f string with several rows so we can re-use it to print/and save the file later.
+election_results= f""" 
 Election Results
 ------------------------------------
 Total Votes: {total_num_votes}
@@ -41,7 +44,7 @@ Winner: {winner[0]}
 -----------------------------------
 """
 
-print(election_results)
-textfile_path = os.path.join(".","Analysis", "elections_results.txt") 
+print(election_results) #display election results
+textfile_path = os.path.join(".","Analysis", "elections_results.txt")  #path to a folder where it will be saved
 with open(textfile_path, "w") as text_file:
-    text_file.write(election_results)
+    text_file.write(election_results) 
